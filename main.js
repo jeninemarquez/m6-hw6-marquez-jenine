375e5e6cd85391266aa22c8d49aaab3fc258bf2e
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-02-16 18:36:29 -0500

    Set up HTML and JS files

---
+++ main.js
@@ -0,0 +1,9 @@
+//When a user hovers over any one of the boxes, highlight all the boxes with a different background color
+
+//When a user clicks on box #1, When a user clicks on box #1, have a message appear somewhere on the page that says “Oooh - so close, but no cigar” by appending a new DOM node
+
+//When a user clicks on box #2, have a message appear somewhere on the page that says “DING DING DING - We have a winner” by appending a new DOM node
+
+//When a user clicks on box #3, have a message appear somewhere on the page that says “Oops, butter luck next time” by appending a new DOM node
+
+//When any one of the three boxes are clicked, have a button that says “Start Over” with an href of “#” appear somewhere on the page by removing a hidden class
