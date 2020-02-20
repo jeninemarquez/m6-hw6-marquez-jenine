6172644c2ce8525f5599c262bb81ce675234451d
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-02-20 11:22:15 -0500

    Add messages on click for each box

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //When a user hovers over any one of the boxes, highlight all the boxes with a different background color
 
-//When a user clicks on box #1, When a user clicks on box #1, have a message appear somewhere on the page that says “Oooh - so close, but no cigar” by appending a new DOM node
 
+//When a user clicks on box #1, When a user clicks on box #1, have a message appear somewhere on the page that says “Oooh - so close, but no cigar.” by appending a new DOM node
+document.getElementById("box-one").addEventListener("click", function (){
+    document.getElementById("text").innerText = "Oooh - so close, but no cigar.";
+});
 //When a user clicks on box #2, have a message appear somewhere on the page that says “DING DING DING - We have a winner” by appending a new DOM node
-
+document.getElementById("box-two").addEventListener("click", function (){
+    document.getElementById("text").innerText = "DING DING DING - We have a winner!";
+});
 //When a user clicks on box #3, have a message appear somewhere on the page that says “Oops, butter luck next time” by appending a new DOM node
-
+document.getElementById("box-three").addEventListener("click", function (){
+    document.getElementById("text").innerText = "Oops, better luck next time!";
+});
 //When any one of the three boxes are clicked, have a button that says “Start Over” with an href of “#” appear somewhere on the page by removing a hidden class
